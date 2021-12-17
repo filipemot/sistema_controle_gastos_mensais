7ef83774ee947415012ed88877f642f6d37f449b
Luis Filipe Guedes Mota <filipemot@gmail.com>
2021-12-17 15:21:21 -0300

    Update records with acents problems

---
+++ backend/src/main/resources/db/changelog/202112171518_update_conta_problemas_acentos.sql
@@ -0,0 +1,7 @@
+update contas set descricao='Café' where descricao ='CafÃ©';
+update contas set descricao='Pão' where descricao ='PÃ£o';
+update contas set descricao='Açougue' where descricao ='AÃ§ougue';
+update contas set descricao='Água' where descricao ='Ã�gua';
+update contas set descricao='Almoço - São Geraldo' where descricao ='AlmoÃ§o - SÃ£o Geraldo';
+update contas set descricao='Bola - Parque da Mônica' where descricao ='Bola - Parque da MÃ´nica';
+update contas set descricao='Armazém' where descricao ='ArmazÃ©m';
