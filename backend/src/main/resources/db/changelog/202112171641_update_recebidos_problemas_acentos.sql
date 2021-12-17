9a3b489d796f654adc8ba3e46d2ef5cc0a1347f5
Luis Filipe Guedes Mota <filipemot@gmail.com>
2021-12-17 16:43:01 -0300

    Update records with acents problems

---
+++ backend/src/main/resources/db/changelog/202112171641_update_recebidos_problemas_acentos.sql
@@ -0,0 +1,3 @@
+update recebidos set descricao='Salário Tria' where descricao ='SalÃ¡rio Tria';
+update recebidos set descricao='Salário Denise' where descricao ='SalÃ¡rio Denise';
+update recebidos set descricao='Previdência Privada' where descricao ='PrevidÃªncia Privada';
